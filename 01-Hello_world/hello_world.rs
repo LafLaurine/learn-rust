bf8e571b856042f1a5c9af66f64071ddfb2bbe92
LafLaurine <laurine.lafontaine@outlook.fr>
2021-06-27 15:04:46 +0200

    01-Hello Rust and Cargo

---
+++ 01-Hello_world/hello_world.rs
@@ -0,0 +1,7 @@
+/*As every language, Rust possesses a main function. It is always the first code that runs in every executable Rust program. */
+
+/*println! calls a Rust macro. (!) indicates that is a macro rather than a function call.*/
+
+fn main() {
+    println!("Hello, world!");
+}
